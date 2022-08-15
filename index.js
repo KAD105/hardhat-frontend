5754fb085ece459b0aa882a5a2a4b7d16e056358
root <root@DESKTOP-U112ORJ.localdomain>
2022-08-15 08:04:18 +0200

    refactor: index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+
+const connectBtn = document.querySelector(".connect-btn"); 
+const fundBtn = document.querySelector(".fund__btn");
 
 
-async function connect(){
-    if (typeof window.ethereum !== "undefined"){
-        await window.ethereum.request({method: "eth_requestAccounts"})
-    } else{
-        console.log("No metamask");
-    }
-}
+ connectBtn.addEventListener("click", async function connect(){
+    if (typeof window.ethereum !== "undefined") {
+        try {
+          await ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+          console.log(error)
+        }
+        connectBtn.innerHTML = "Connected"
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+        console.log(accounts)
+      } else {
+        connectBtn.innerHTML = "Please install MetaMask"
+      }
+ })
+
 
