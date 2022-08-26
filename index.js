50fdbff23cf5d688160d1aed6b3fb58aecfe531f
root <root@DESKTOP-U112ORJ.localdomain>
2022-08-26 18:32:13 +0200

    refactor: index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
+import { contractAddress, abi } from "./constants.js"; 
 
 const connectBtn = document.querySelector(".connect-btn"); 
 const fundBtn = document.querySelector(".fund__btn");
 
 
- connectBtn.addEventListener("click", async function connect(){
+connectBtn.addEventListener("click", async function connect(){
     if (typeof window.ethereum !== "undefined") {
         try {
           await ethereum.request({ method: "eth_requestAccounts" })
@@ -17,14 +18,38 @@ const fundBtn = document.querySelector(".fund__btn");
       } else {
         connectBtn.innerHTML = "Please install MetaMask"
       }
- })
+})
 
- fundBtn.addEventListener("click", async function fund(){
+fundBtn.addEventListener("click", async function fund(){
+  const ethAmount = "0.1";
+  console.log(`funding with ${ethAmount}`);
+
   if(typeof window.ethereum !== "undefined"){
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    console.log(signer);
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try{
+      const transactionResponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      })
+      await listenForTxToBeMined(transactionResponse, provider);
+      console.log("done!....yay");
+      
+    }catch(error){
+      console.log(error);
+    }
     
-  }
- })
+  } 
+})
+
+function listenForTxToBeMined(transactionResponse, provider){
+  console.log(`Mining ${transactionResponse.hash} ...`);
+  return new Promise ((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionReceipt)=>{
+      console.log(`Completed with ${transactionReceipt.confirmations} confirmations.`);
+      resolve()
+    })
+
+  })
+}
 
