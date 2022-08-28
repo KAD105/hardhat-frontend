4f78c00aab09dd5621e8739695e0cb98b5e22b50
root <root@DESKTOP-U112ORJ.localdomain>
2022-08-28 08:01:42 +0200

    add: get the balance functionality added

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ import { contractAddress, abi } from "./constants.js";
 
 const connectBtn = document.querySelector(".connect-btn"); 
 const fundBtn = document.querySelector(".fund__btn");
+const balanceBtn = document.querySelector(".balance__btn");
+const balanceAmount = document.getElementById("balance__amount");
 
 
 connectBtn.addEventListener("click", async function connect(){
     if (typeof window.ethereum !== "undefined") {
-        try {
-          await ethereum.request({ method: "eth_requestAccounts" })
-        } catch (error) {
-          console.log(error)
-        }
-        connectBtn.innerHTML = "Connected"
-        const accounts = await ethereum.request({ method: "eth_accounts" })
-        console.log(accounts)
-      } else {
+      try {
+        await ethereum.request({ method: "eth_requestAccounts" })
+      } catch (error) {
+        console.log(error)
+      }
+      connectBtn.innerHTML = "Connected"
+      const accounts = await ethereum.request({ method: "eth_accounts" })
+      console.log(accounts)
+    } else {
         connectBtn.innerHTML = "Please install MetaMask"
       }
 })
 
 fundBtn.addEventListener("click", async function fund(){
-  const ethAmount = "0.1";
+  const ethAmount = document.getElementById("eth-amount").value;
   console.log(`funding with ${ethAmount}`);
 
   if(typeof window.ethereum !== "undefined"){
@@ -32,18 +34,25 @@ fundBtn.addEventListener("click", async function fund(){
       const transactionResponse = await contract.fund({
         value: ethers.utils.parseEther(ethAmount),
       })
-      await listenForTxToBeMined(transactionResponse, provider);
+      await listenForTxToBeMined(transactionResponse, provider, ethAmount);
       console.log("done!....yay");
+      alert(`You have successfuly funded ${ethAmount} ETH.`);
+
+      // Clearing the input field after the submission
+      // ethAmount.value = "";
       
     }catch(error){
       console.log(error);
     }
     
-  } 
+  } else {
+    fundBtn.innerHTML = "Please install Metamask!"
+  }
 })
 
-function listenForTxToBeMined(transactionResponse, provider){
+function listenForTxToBeMined(transactionResponse, provider, ethAmount){
   console.log(`Mining ${transactionResponse.hash} ...`);
+  
   return new Promise ((resolve, reject) => {
     provider.once(transactionResponse.hash, (transactionReceipt)=>{
       console.log(`Completed with ${transactionReceipt.confirmations} confirmations.`);
@@ -52,4 +61,21 @@ function listenForTxToBeMined(transactionResponse, provider){
 
   })
 }
+
+balanceBtn.addEventListener("click", async function getTheBalance (){
+  if (typeof window.ethereum !== "undefined"){
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    try{
+      const balanceInWei = await provider.getBalance(contractAddress);
+      const balanceInEth = ethers.utils.formatEther(balanceInWei)
+      console.log(balanceInEth);
+      balanceAmount.innerHTML = balanceInEth
+    } catch(error){
+      console.log(error);
+    }
+  } else {
+    balanceBtn.innerHTML = "Please install Metamask!"
+  }
+})
+
 
